98da468e783005880933c30e719a2a4cbe262969
Anshu Sinha <anshujuly2@gmail.com>
2023-04-05 10:45:48 +0530

    Add a code on working with pandas dataframe and series, working with columns and rows

--- main.py
+++ main.py
@@ -18,4 +18,63 @@ import pandas
 
 """using pandas module to read a csv file"""
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(data["temp"])
+
+# """converting the data to dictionary format"""
+# dataDict = data.to_dict()
+# print(dataDict)
+
+# """converting the data to a list"""
+# tempList = data["temp"].to_list()
+# sum = 0
+# for temp in tempList:
+#     sum += temp
+# avgTemp = sum / len(tempList)
+# print(round(avgTemp, 2))
+
+# avgTemp = sum(tempList) / len(tempList)
+# print(round(avgTemp, 2))
+
+# """getting the avg temperature"""
+# print(
+#     round(
+#         data["temp"].mean(), 2
+#     )
+# )
+#
+# """getting the maximum value from the data's temp column"""
+# print(
+#     data["temp"].max()
+# )
+#
+# """getting the data in a column"""
+# print(
+#     data.condition
+# )
+
+# """getting the data in a row"""
+# print(
+#     data[data.day == "Monday"]
+# )
+
+# print(
+#     data[data.temp == data.temp.max()]
+# )
+
+# """getting the condition of a single row"""
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+
+# """creating a dataframe from scratch"""
+# dataDict = {
+#     "students": ["Anuragini", "Arup", "Bibek", "Aadarsh"],
+#     "age": [19, 20, 21, 21]
+# }
+#
+# data2 = pandas.DataFrame(dataDict)
+# print(data2)
+#
+# """converting the obtaines data to csv format"""
+# print(
+#     data2.to_csv()
+# )
